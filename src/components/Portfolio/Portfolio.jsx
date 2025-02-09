6bacaddaf5534c064a43c86dccbb3626d9d63a7e
RayyanDevZone <rayyansyed369@gmail.com>
2025-02-09 11:36:10 +0530

    7th page done

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,9 +1,81 @@
 
+import { IoLocationOutline } from "react-icons/io5";
+
+const cardsData = [
+  {
+    title: "Azila Mansion",
+    subtitle: "Luxury Residences, Modern Homes",
+    location: "Dubai Marina",
+    image: "https://html.themewant.com/elever/assets/images/portfolio/05.webp",
+    details: {
+      Price: "From $290k",
+      Client: "RC Builders",
+      "Project year": "15 Oct 2025",
+      Duration: "3Y, 4M"
+    }
+  },
+  {
+    title: "Greenfield Eco Community",
+    subtitle: "Luxury Residences, Modern Homes",
+    location: "Berlin, Germany",
+    image: "https://html.themewant.com/elever/assets/images/portfolio/06.webp",
+    details: {
+      Price: "From $290k",
+      Client: "RC Builders",
+      "Project year": "15 Oct 2025",
+      Duration: "3Y, 4M"
+    }
+  }
+];
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
-  )
-}
+    <div className="min-h-screen h-auto flex flex-col sm:px-18 bg-[#EFEBE7] font-lexend">
+      <div className="w-full flex flex-row justify-between items-center mt-20">
+        <div>
+          <p className="text-5xl font-bold text-[#1D1D1F]">
+          What we create last 5+ years<br/>
+          for our customers
+          </p>
+          <p className="text-sm mt-5 text-[#999999]">
+            Quis nulla blandit vulputate morbi adipiscing sem vestibulum. Nulla
+            turpis integer dui sed <br /> posuere sem. Id molestie mi arcu
+            gravida lorem potenti.
+          </p>
+        </div>
+        <button className="bg-[#F84E1D] cursor-pointer text-white px-8 py-3 tracking-tighter rounded-md font-semibold text-xl">
+          More Portfolio
+        </button>
+      </div>
 
-export default Portfolio
+      <div className="h-auto flex flex-row justify-between mt-10 mb-20">
+        {cardsData.map((card, index) => (
+          <div key={index} className="card w-[550px] h-[600px] rounded-2xl flex flex-col bg-white">
+            <div
+              className="w-full h-3/5 rounded-t-2xl bg-cover bg-center px-12 py-8 flex flex-col justify-between text-white"
+              style={{ backgroundImage: `url(${card.image})` }}
+            >
+              <div>
+                <p className="text-3xl font-bold">{card.title}</p>
+                <p className="text-sm">{card.subtitle}</p>
+              </div>
+              <p className="border border-white px-3 backdrop-blur-md py-2 w-40 rounded-md flex items-center justify-center">
+                <IoLocationOutline className="mr-2" /> {card.location}
+              </p>
+            </div>
+            <div className="h-2/5 w-full flex flex-row justify-center flex-wrap gap-4 p-4">
+              {Object.entries(card.details).map(([key, value], index) => (
+                <div key={index} className="h-auto w-[130px] rounded-lg bg-[#F5f5f5] border border-[#EDECEC] flex flex-col px-2 py-2">
+                  <p className="text-[#999999]">{key}:</p>
+                  <p className="text-xl font-semibold">{value}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
