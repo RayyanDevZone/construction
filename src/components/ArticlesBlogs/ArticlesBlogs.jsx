5c8e6dccbfd477344449abfe9ff29d48a90716bd
RayyanDevZone <rayyansyed369@gmail.com>
2025-02-09 22:49:49 +0530

    responsive 5

--- src/components/ArticlesBlogs/ArticlesBlogs.jsx
+++ src/components/ArticlesBlogs/ArticlesBlogs.jsx
@@ -1,13 +1,13 @@
 const ArticlesBlogs = () => {
   return (
-    <div className="min-h-screen h-auto w-full flex flex-col px-12 py-16 bg-[#F5F3F2] font-lexend">
-      <div className="w-full h-[120px] border-b-1 border-b-[#EBE4E0] flex flex-row justify-between items-center">
+    <div className="min-h-screen h-auto w-full flex flex-col sm:px-12 px-4 py-16 bg-[#F5F3F2] font-lexend">
+      <div className="w-full min-h-[120px] border-b-1 border-b-[#EBE4E0] flex sm:flex-row flex-col justify-between sm:items-center items-start">
         <div>
           <p className="uppercase text-[#f84e1d] font-bold">
             {" "}
             services we provide
           </p>
-          <p className="uppercase text-[#141416] text-5xl mb-8 mt-4 font-bold">
+          <p className="uppercase text-[#141416] sm:text-5xl text-2xl mb-8 mt-4 font-bold">
             Articles & Blog Posts
           </p>{" "}
         </div>
@@ -16,7 +16,7 @@ const ArticlesBlogs = () => {
         </button>
       </div>
 
-      <div className="w-full mx-5 flex flex-col px-4 py-5 min-h-[800px] items-center  bg-white rounded-xl mt-16 ">
+      <div className="w-full sm:mx-5 flex flex-col px-4 py-5 sm:min-h-[800px] items-center  bg-white rounded-xl mt-16 ">
         <p className="uppercase border-b-1 border-[#EBE4E0] flex items-center justify-center w-full pb-4">
           Through a unique combination of engineering, and construction
         </p>
@@ -25,7 +25,7 @@ const ArticlesBlogs = () => {
           className="w-full rounded-xl my-3"
         ></img>
         <div className="w-full border-t-1 border-[#EBE4E0] h-[200px] flex flex-row items-center ">
-          <div className="h-full flex flex-col pl-5 font-bold justify-center w-[150px] border-r-1 border-[#EBE4E0]">
+          <div className="h-full sm:flex hidden flex-col sm:pl-5 font-bold justify-center w-[150px] border-r-1 border-[#EBE4E0]">
             <img
               src="https://html.themewant.com/elever/assets/images/blog/18.webp"
               className="h-[70px] w-[70px]"
@@ -33,12 +33,12 @@ const ArticlesBlogs = () => {
             <p className="mt-5 text-start">BY</p>
             <p className="text-start">Markus Smith</p>
           </div>
-          <div className="flex flex-col px-5 py-7">
+          <div className="flex flex-col sm:px-5 py-7">
             <div className=" flex flex-row items-center gap-8 uppercase ">
               <p className="text-[#1d1d1f]">18 December, 2024 </p>
               <p className="text-[#f84e1d] ">Interior Design</p>
             </div>{" "}
-            <p className="text-2xl font-bold text-[#1d1d1f] mt-5 uppercase hover:text-[#f84e1d] cursor-pointer">
+            <p className="sm:text-2xl font-bold text-[#1d1d1f] mt-5 uppercase hover:text-[#f84e1d] cursor-pointer">
               construction and design disciplines and expertise,liquid
               <br /> delivers excellent design perspective through
             </p>
--- src/components/CompaniesUse/CompaniesUse.jsx
+++ src/components/CompaniesUse/CompaniesUse.jsx
@@ -23,14 +23,14 @@ const companiesData = [
 
 const CompaniesUse = () => {
   return (
-    <div className="min-h-screen h-auto w-full bg-[#EFEBE7] flex flex-col px-16 justify-between py-5 font-lexend">
-      <p className="text-4xl font-bold text-[#1D1D1F]">
+    <div className="min-h-screen h-auto w-full bg-[#EFEBE7] flex flex-col sm:px-16 px-8 justify-between py-5 font-lexend">
+      <p className="sm:text-4xl text-2xl font-bold text-[#1D1D1F]">
         <span className="text-[#f84e1d]">156+ </span>
         companies use Elever to<br /> power their integrations
       </p>
-      <div className="card-container flex flex-row w-full mb-10 gap-8 mt-8">
+      <div className="card-container flex sm:flex-row flex-col w-full mb-10 gap-8 mt-8">
         {companiesData.map((company, index) => (
-          <div key={index} className="card h-[300px] w-[260px] bg-white rounded-xl flex flex-col items-center justify-between  p-4">
+          <div key={index} className="card h-[300px] sm:w-[260px] w-[350px] bg-white rounded-xl flex flex-col items-center justify-between  p-4">
             <div className="h-[40px] w-[200px]  bg-cover bg-center mt-8" style={{ backgroundImage: `url(${company.image})` }}></div>
             <div className="h-[2px] rounded-full mt-4 w-full bg-[#f84e1d]"></div>
             <div className="flex flex-col h-2/3  mt-4">
