2088b46de8bb17cf6c4343af0db3359d858a7f69
RayyanDevZone <rayyansyed369@gmail.com>
2025-02-09 20:36:47 +0530

    ui updates

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,25 +1,34 @@
 const testimonialsData = [
   {
-    image: "https://html.themewant.com/elever/assets/images/testimonials/06.webp",
+    image:
+      "https://html.themewant.com/elever/assets/images/testimonials/06.webp",
     name: "Daniel Kobak",
     company: "MARKTWO.INC",
-    feedback: "The innovation designs presented exceeded our expectations. Their creative approach and attention to details were evident in every aspect of the project. Aesthetics showcased their commitments.",
-    brandImage: "https://html.themewant.com/elever/assets/images/testimonials/brand/01.webp"
+    feedback:
+      "The innovation designs presented exceeded our expectations. Their creative approach and attention to details were evident in every aspect of the project. Aesthetics showcased their commitments.",
+    brandImage:
+      "https://html.themewant.com/elever/assets/images/testimonials/brand/01.webp",
   },
   {
-    image: "https://html.themewant.com/elever/assets/images/testimonials/07.webp",
+    image:
+      "https://html.themewant.com/elever/assets/images/testimonials/07.webp",
     name: "Sarah Johnson",
     company: "BUILDTECH",
-    feedback: "Their professional team delivered high-quality work and demonstrated strong project management skills. We are extremely satisfied with the results and highly recommend their services.",
-    brandImage: "https://html.themewant.com/elever/assets/images/testimonials/brand/02.webp"
+    feedback:
+      "Their professional team delivered high-quality work and demonstrated strong project management skills. We are extremely satisfied with the results and highly recommend their services.",
+    brandImage:
+      "https://html.themewant.com/elever/assets/images/testimonials/brand/02.webp",
   },
   {
-    image: "https://html.themewant.com/elever/assets/images/testimonials/08.webp",
+    image:
+      "https://html.themewant.com/elever/assets/images/testimonials/08.webp",
     name: "Michael Lee",
     company: "CONSTRUCTO",
-    feedback: "The project was completed on time and within budget. Their innovative solutions and expertise helped us achieve our goals efficiently. We appreciate their dedication and hard work.",
-    brandImage: "https://html.themewant.com/elever/assets/images/testimonials/brand/03.webp"
-  }
+    feedback:
+      "The project was completed on time and within budget. Their innovative solutions and expertise helped us achieve our goals efficiently. We appreciate their dedication and hard work.",
+    brandImage:
+      "https://html.themewant.com/elever/assets/images/testimonials/brand/03.webp",
+  },
 ];
 
 const Testimonials = () => {
@@ -32,9 +41,12 @@ const Testimonials = () => {
         </p>
       </div>
 
-      <div className="card-container w-full flex overflow-x-auto whitespace-nowrap  hide-scrollbar mt-10 gap-16">
+      <div className="card-container w-full flex overflow-x-auto whitespace-nowrap  hideScrollbar mt-10 gap-16">
         {testimonialsData.map((testimonial, index) => (
-          <div key={index} className="card h-[500px] w-[500px] ml-10 flex-shrink-0 flex flex-col items-start bg-white overflow-hidden shadow-lg rounded-xl">
+          <div
+            key={index}
+            className="card h-[500px] w-[500px] ml-10 flex-shrink-0 flex flex-col items-start bg-white overflow-hidden shadow-lg rounded-xl"
+          >
             <div className="flex flex-row items-center gap-6 ">
               <img
                 src={testimonial.image}
@@ -42,12 +54,22 @@ const Testimonials = () => {
                 className="h-[150px]"
               />
               <div>
-                <p className="text-2xl font-bold text-[#141416]">{testimonial.name}</p>
-                <p className="text-sm mt-2 text-[#f84e1d] font-semibold">{testimonial.company}</p>
+                <p className="text-2xl font-bold text-[#141416]">
+                  {testimonial.name}
+                </p>
+                <p className="text-sm mt-2 text-[#f84e1d] font-semibold">
+                  {testimonial.company}
+                </p>
               </div>
             </div>
-            <p className="text-[#999999] px-10 text-lg mt-12 break-words whitespace-normal">{testimonial.feedback}</p>
-            <img src={testimonial.brandImage} alt="testimonials" className="h-[40px] mt-12 ml-10" />
+            <p className="text-[#999999] px-10 text-lg mt-12 break-words whitespace-normal">
+              {testimonial.feedback}
+            </p>
+            <img
+              src={testimonial.brandImage}
+              alt="testimonials"
+              className="h-[40px] mt-12 ml-10"
+            />
           </div>
         ))}
       </div>
