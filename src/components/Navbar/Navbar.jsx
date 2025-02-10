597d69a859e543d44756017d16c619be91b24968
RayyanDevZone <rayyansyed369@gmail.com>
2025-02-10 13:59:04 +0530

    icons in menu bar added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
+import { FaFacebookF, FaYoutube, FaLinkedinIn } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 import { RiMenu4Line, RiCloseLine } from "react-icons/ri";
 
+
+
+const socialIcons = [FaFacebookF, FaXTwitter, FaYoutube, FaLinkedinIn];
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
@@ -29,7 +34,7 @@ const Navbar = () => {
       <div className={`fixed top-0 right-0 h-full w-4/5 bg-white shadow-lg transform ${menuOpen ? "translate-x-0" : "translate-x-full"} transition-transform duration-300 ease-in-out`}>
         <div className="flex items-center justify-between p-4 border-b border-gray-200">
           <h2 className="text-xl font-bold">Menu</h2>
-          <p onClick={toggleMenu} className="cursor-pointer text-3xl">
+          <p onClick={toggleMenu} className="cursor-pointer text-[#f84e1d] text-3xl">
             <RiCloseLine />
           </p>
         </div>
@@ -37,6 +42,13 @@ const Navbar = () => {
           {["Home", "About", "Services", "Pages", "Shop", "Blog", "Contact"].map((item, index) => (
             <p key={index} className="font-semibold text-[#999999] text-lg/8 cursor-pointer">{item}</p>
           ))}
+          <div className="flex gap-6 mt-6">
+            {socialIcons.map((Icon, index) => (
+              <button key={index} className="h-[40px] w-[40px] flex items-center justify-center rounded-md border border-[#2F3336] text-white bg-[#171717]">
+                <Icon />
+              </button>
+            ))}
+          </div>
         </div>
       </div>
     </div>
