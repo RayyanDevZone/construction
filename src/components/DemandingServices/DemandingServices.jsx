4bb58e9193bc45c85ddfd64bd42cb554a42aee1e
RayyanDevZone <rayyansyed369@gmail.com>
2025-02-21 21:59:36 +0530

    demand animation

--- src/components/DemandingServices/DemandingServices.jsx
+++ src/components/DemandingServices/DemandingServices.jsx
@@ -37,18 +37,37 @@ const DemandingServices = () => {
       duration:1
     })
   })
+  useGSAP(()=>{
+    gsap.from(".head , .mdhead",{
+      opacity:0,
+      x:-100,
+      scrollTrigger:{ trigger:".head",start:"top 80%",},
+      duration:.7,
+      stagger:.3
+    })
+  })
+  useGSAP(()=>{
+    gsap.from(".btn-demand",{
+      opacity:0,
+      x:100,
+      scrollTrigger:{ trigger:".btn-demand",start:"top 80%",},
+      duration:.7,
+      stagger:.3
+    })
+  })
+
   return (
     <div className="w-full min-h-screen h-auto bg-[#EDE9E5]">
       <div className="h-[200px] w-full  flex sm:flex-row flex-col justify-between sm:items-center items-start sm:px-16 px-4 py-20  font-lexend ">
         <div>
-          <p className="sm:text-4xl text-2xl font-bold">We Provide Demanding Services</p>
-          <p className="mt-3 text-sm text-[#999999]">
+          <p className=" head sm:text-4xl text-2xl font-bold">We Provide Demanding Services</p>
+          <p className=" mdhead mt-3 text-sm text-[#999999]">
             Quis nulla blandit vulputate morbi adipiscing sem vestibulum. Nulla
             turpis integer dui sed
             <br /> posuere sem. Id molestie mi arcu gravida lorem potenti.
           </p>
         </div>
-        <button className="bg-[#F84E1D] cursor-pointer text-white px-8 py-3 my-3 tracking-tighter rounded-md font-semibold text-lg flex flex-row items-center">
+        <button className="btn-demand bg-[#F84E1D] cursor-pointer text-white px-8 py-3 my-3 tracking-tighter rounded-md font-semibold text-lg flex flex-row items-center">
           Book Appointment <FaLocationArrow className="ml-3" />
         </button>
       </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,9 +2,28 @@ import { useState, useEffect } from "react";
 import { FaFacebookF, FaYoutube, FaLinkedinIn } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { RiMenu4Line, RiCloseLine } from "react-icons/ri";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger, useGSAP);
 
 const socialIcons = [FaFacebookF, FaXTwitter, FaYoutube, FaLinkedinIn];
 const Navbar = () => {
+
+  useGSAP(() => {
+    gsap.from(".navbar p", {
+      opacity: 0,
+      stagger: 0.1,
+    });
+  });
+  useGSAP(() => {
+    gsap.from(".btn", {
+      opacity: 0,
+      duration: 0.5,
+    });
+  });
+
   const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
@@ -20,37 +39,76 @@ const Navbar = () => {
   };
 
   return (
-    <div className=" top-0 sm:top-14 bg-white w-full z-999 h-22 flex flex-row items-center sm:px-18 px-4 justify-between font-lexend">
-      <img src="https://html.themewant.com/elever/assets/images/logo/02.svg" alt="Logo" />
+    <div className="navbar top-0 sm:top-14 bg-white w-full z-999 h-22 flex flex-row items-center sm:px-18 px-4 justify-between font-lexend">
+      <img
+        src="https://html.themewant.com/elever/assets/images/logo/02.svg"
+        alt="Logo"
+      />
       <div className="lg:flex flex-row items-center gap-10 hidden">
-        {["Home", "Services", "Pages", "Shop", "Project", "Blog", "Contact"].map((item, index) => (
-          <p key={index} className="font-semibold text-[#141416]">{item}</p>
+        {[
+          "Home",
+          "Services",
+          "Pages",
+          "Shop",
+          "Project",
+          "Blog",
+          "Contact",
+        ].map((item, index) => (
+          <p key={index} className="font-semibold text-[#141416]">
+            {item}
+          </p>
         ))}
       </div>
       <div className="flex flex-row gap-2">
-        <button className="hidden md:block bg-[#F84E1D] cursor-pointer text-white px-8 py-4 tracking-tighter rounded-lg font-semibold text-xl">
+        <button className=" btn hidden md:block bg-[#F84E1D] cursor-pointer text-white px-8 py-4 tracking-tighter rounded-lg font-semibold text-xl">
           Request a Quote
         </button>
-        <p onClick={toggleMenu} className="border border-[#1D1D1F] w-14 cursor-pointer text-3xl rounded-lg flex items-center justify-center h-[55px]">
+        <p
+          onClick={toggleMenu}
+          className="border border-[#1D1D1F] w-14 cursor-pointer text-3xl rounded-lg flex items-center justify-center h-[55px]"
+        >
           <RiMenu4Line />
         </p>
       </div>
 
       {/* Mobile Menu */}
-      <div className={`fixed z-999 top-0 right-0 h-full w-4/5 bg-white shadow-lg transform ${menuOpen ? "translate-x-0" : "translate-x-full"} transition-transform duration-300 ease-in-out`}>
+      <div
+        className={`fixed z-999 top-0 right-0 h-full w-4/5 bg-white shadow-lg transform ${
+          menuOpen ? "translate-x-0" : "translate-x-full"
+        } transition-transform duration-300 ease-in-out`}
+      >
         <div className="flex items-center justify-between p-4 border-b border-gray-200">
           <h2 className="text-xl font-bold">Menu</h2>
-          <p onClick={toggleMenu} className="cursor-pointer text-[#f84e1d] text-3xl">
+          <p
+            onClick={toggleMenu}
+            className="cursor-pointer text-[#f84e1d] text-3xl"
+          >
             <RiCloseLine />
           </p>
         </div>
         <div className="flex flex-col p-4 px-12  space-y-4">
-          {["Home", "About", "Services", "Pages", "Shop", "Blog", "Contact"].map((item, index) => (
-            <p key={index} className="font-semibold text-[#999999] text-lg/8 cursor-pointer active:text-[#f84e1d] duration-100">{item}</p>
+          {[
+            "Home",
+            "About",
+            "Services",
+            "Pages",
+            "Shop",
+            "Blog",
+            "Contact",
+          ].map((item, index) => (
+            <p
+              key={index}
+              className="font-semibold text-[#999999] text-lg/8 cursor-pointer active:text-[#f84e1d] duration-100"
+            >
+              {item}
+            </p>
           ))}
           <div className="flex gap-6 mt-6">
             {socialIcons.map((Icon, index) => (
-              <button key={index} className="h-[40px] w-[40px] flex items-center justify-center rounded-md border border-[#2F3336] text-white bg-[#171717]">
+              <button
+                key={index}
+                className="h-[40px] w-[40px] flex items-center justify-center rounded-md border border-[#2F3336] text-white bg-[#171717]"
+              >
                 <Icon />
               </button>
             ))}
